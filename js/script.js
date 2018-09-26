b63177f3875e35f149cf213c406aca583a4aa7f2
P\lenovo <2427952638@qq.com>
2018-09-26 10:51:01 +0800

    no message

---
+++ js/script.js
@@ -0,0 +1,171 @@
+/*$("#app").html("hello")*/
+/*var app =document.querySelector("#app")
+app.innerHTML="hello"*/
+/*var app = new Vue({
+	el: ".app" ,
+	data: {
+	text:"hello"
+	}
+})*/
+/*new Vue({
+	el:"#app",
+	data:{
+		text:"今天我学习了",
+		message:"vue.js"
+		
+	}
+})*/
+/*new Vue({
+	el:"#app",
+	data:{
+		text:"百度",
+		url:"https://www.baidu.com",
+		title:"vue.js"	
+	}
+})*/
+
+/*new Vue({
+	el:"#app",
+	data:{
+		text:"百度",
+		http:"https://www.baidu.com/"
+	}
+})
+*/
+
+/*new Vue({
+	el:"#app",
+	data:{
+	   message:"页面加载与"+ new Date().toLocaleString()
+	}
+})*/
+
+//false true
+new Vue({
+  el: '#app',
+  data: {
+    seen: false,
+    sen:false
+  }
+})
+/*new Vue({
+	el: '#app',
+	data: {
+		ok: true
+	}
+})*/
+/*new Vue({
+	el: '#app',
+	data: {
+		qq: true
+	}
+})*/
+/*new Vue({
+	el:'#app',
+	data:{
+		arrs: [1,2,3,]
+	}
+})*/
+new Vue({
+	 el:'#app',
+	data:{
+		arrs: [1,2,3,]
+	}
+})     age":70,[
+		{"mz":"陈小艺","age":50},
+		{"mz":"陈小易","age":80}
+		]
+	}
+})
+
+/*new Vue({
+	el:'#app',
+	data:{
+		btn:"按钮"		
+	},
+	methods:{
+		fun:function(){
+			this.btn="加载中"
+		}
+	}
+})*/
+
+/*new Vue({
+	el:'#app',
+	data:{
+		num:0		
+	},
+	methods:{
+		qwe:function(){
+			this.num-=1
+		},
+		asd:function(){
+			this.num+=1
+		},
+		zxc:function(){
+		   alert(this.num)
+		},
+		fun:function(){
+			console.log(21)
+		},
+		un:function(){
+			console.log("zhx")
+		}
+	}
+})*/
+
+/*new Vue({
+	el:'#app',
+	data:{
+		num:1,
+		ok:true,
+		num1:2 
+	},
+	computed:{
+		Yes:function(){
+			return this.num+this.num1
+		}
+	}
+})*/
+/*new Vue({
+	el: '#app',
+	data: {
+		num1: "htr"
+	},
+	methods: {
+		btn: function() {
+			this.num1 +="htrt";
+		}
+	},
+	computed: {
+		num2: function() {
+			return this.num1 + "utghkjgujgr"
+		}
+	}
+})*/
+
+/*new Vue({
+	el: '#app',
+	data: {
+		hello:"dsf",
+		bool: true
+	}
+})*/
+/*new Vue({
+	el: '#app',
+	data: {
+		hello:"asd",
+		al:{
+			qq:true,
+			ww:true,
+			ee:true,
+		},
+	}
+})*/
+new Vue({
+	el: '#app',
+	data: {
+		message:""
+
+	}
+})
